f06a5e078acf0ec767122d7500c846218d1c3af3
coletree <coletree@gmail.com>
2024-04-26 20:52:59 +0800

    Ultimate Portfolio

--- UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
@@ -5,13 +5,51 @@
 //  Created by coletree on 2024/4/23.
 //
 
+import CoreData
 import SwiftUI
+
+
+
 
 @main
 struct UltimatePortfolioApp: App {
+    
+    
+    //MARK: - 属性
+    
+    //状态属性：实作视图模型 DataController
+    @State var dataController = DataController()
+    
+    
+    
+    
+    //MARK: - 根视图
     var body: some Scene {
         WindowGroup {
             ContentView()
+                //把视图模型 dataController 放入环境中
+                .environmentObject(dataController)
+                //把 dataController 中的上下文，赋予环境变量 managedObjectContext
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
+    
+    
+    
+    
 }
+
+
+
+
+/*
+ 本项目分为三个部分，分别是：
+ 1.构建核心应用程序，我们将在其中查看 SwiftUI、核心数据、架构、测试、可访问性、文档、网络、本地化等。
+ 2.与系统集成，我们将研究 StoreKit、Core Spotlight、小部件、通知、触觉等。
+ 3.针对每个平台进行设计，我们将扩展我们的基础 iOS 应用程序，以便在 macOS 和 watchOS 上同样运行。
+ 
+ - 只有第1部分是必须的。每个人都必须完全按照第一部分的顺序进行操作，这很重要，因为这是我们构建应用程序核心的地方。
+ - 完成第1部分后，您可以按照您想要的任何顺序完成第 2 部分或第 3 部分中的任何或全部 - 它们都是可选的，并且或多或少都是独立的。
+ - 整个项目适用于 iOS 16、macOS Ventura 和 watchOS 9 的 Xcode 14 ，但它在未来的版本中应该会很好用。
+ - 建议您拥有 Apple 开发者计划的活跃帐户，但我认为除了 StoreKit 等之外不需要它。
+*/
---
+++ UltimatePortfolio/UltimatePortfolio/ViewModel/DataController.swift
@@ -0,0 +1,161 @@
+//
+//  DataController.swift
+//  UltimatePortfolio
+//
+//  Created by coletree on 2024/4/26.
+//
+
+import CoreData
+import Foundation
+
+
+
+//创建控制数据的类：它将处理加载数据，本地保存数据，同步到CloudKit，等工作
+//这里要用 ObservableObject，不能用 Observable ，不然后面的 objectWillChange 属性就无法用
+class DataController : ObservableObject {
+    
+    
+    //MARK: - 属性
+    
+    //常量属性：创建容器，加载 Model 数据模型
+    //let container = NSPersistentContainer(name: "Model")
+    
+    //常量属性：创建一个 CloudKit 的容器（前面已经在CoreData编辑器的配置中勾选了 used with Cloudkit）
+    let container : NSPersistentCloudKitContainer
+    
+    
+
+    
+    //MARK: - 初始化方法：
+    init(inMemory: Bool = false) {
+        
+        //给容器属性赋值（后面 Model 就是告诉程序要加载的数据库）
+        container = NSPersistentCloudKitContainer(name: "Model")
+        
+        //TODO: - 没搞懂
+        if inMemory{
+            container.persistentStoreDescriptions.first?.url = URL(filePath: "/dev/null")
+        }
+        
+        //容器读取数据库中的数据
+        container.loadPersistentStores {
+            storeDescription, error in
+            //如果发生错误，则退出程序。error 是 optional 类型，所以用 if let 解包
+            if let error{
+                fatalError("Fatal error loading store: \(error.localizedDescription)")
+            }
+        }
+        
+    }
+    
+    //方法: 将上下文中的 live 数据，持久化存储下来。存储前先检查是否有变化，节省资源
+    func save(){
+        if container.viewContext.hasChanges{
+            try? container.viewContext.save()
+        }
+    }
+    
+    //方法: 删除实体对象。这里只限制对象要符合 NSManagedObject 协议，这样不管是 issue 还是 tag ，都可以用这个方法
+    func delete(_ object: NSManagedObject){
+        //当对象发生变化时，会向外界发出通知
+        objectWillChange.send()
+        //删除传入的对象
+        container.viewContext.delete(object)
+        //持久化保存下来
+        save()
+    }
+    
+    
+    
+    
+    //MARK: - 测试专用
+    
+    //静态属性：为了方便后面视图的预览，创建一下预处理数据。
+    static var preview: DataController = {
+        let dataController = DataController(inMemory: true)
+        dataController.creatSampleData()
+        return dataController
+    }()
+    
+    //方法: 添加测试数据
+    func creatSampleData(){
+        
+        //常量属性：获取容器的上下文，必须有上下文才能创建/修改数据，context会把数据存到内存
+        let viewContext = container.viewContext
+        
+        for i in 1...5 {
+            
+            //生成 tag
+            let tag = Tag(context: viewContext)
+            tag.id = UUID()
+            tag.name = "Tag \(i)"
+            
+            for j in 1...10{
+                //生成 issue
+                let issue = Issue(context: viewContext)
+                issue.title = "Issue: \(i) - \(j)"
+                issue.content = "问题的描述在这里"
+                issue.completed = Bool.random()
+                issue.priority = Int16.random(in: 0...2)
+                issue.creationDate = .now
+                //modificationDate 已经设置成了派生属性，由程序自动更新，无需自行设置
+                //issue.modificationDate = .now
+                
+                //这个 addToIssues 方法是程序自动生成的实体类里带的
+                tag.addToIssues(issue)
+            }
+        }
+        
+        //使用上下文 viewContext，调用 save 方法将数据写入持久化存储设备
+        try? viewContext.save()
+        
+    }
+    
+    //方法: 创建批量删除所有数据的请求，以方便测试
+    /*
+     1. 在 Issue 上使用 FetchRequest 方法获取所有的 issue 实体对象（不需要指定任何过滤器）
+     2. 将该获取请求包装在【批量删除请求】中，该请求告诉 Core Data 删除与请求匹配的所有对象，即所有 issue
+     3. 在视图上下文上执行批量删除请求
+     有个问题就是：运行批量删除请求需要读出每个已删除对象的核心数据标识符，然后将它们合并到实时视图上下文中。
+     这比预期的工作量要多，所以我们将这部份单独封装在新的 delete() 方法中，这样至少可以重用。
+    */
+    private func delete(_ fetchRequest: NSFetchRequest<NSFetchRequestResult>){
+        
+        //创建批量删除的请求：
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        //用什么来作为批量删除的唯一识别符呢？这里指定是 ObjectIDs
+        batchDeleteRequest.resultType = .resultTypeObjectIDs
+        
+        //然后让上下文执行【批量删除请求】，其执行后的返回结果转型为 NSBatchDeleteResult，赋值给 delete
+        //delete 是 optional 类型，因此这里做解包，解包成功后才执行后续代码
+        if let delete = try? container.viewContext.execute(batchDeleteRequest) as? NSBatchDeleteResult{
+            
+            //changes 是一个字典 [NSDeletedObjectsKey: [ID数组] ]
+            let changes = [NSDeletedObjectsKey: delete.result as? [NSManagedObjectID] ?? [] ]
+            //FIXME: 合并
+            NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [container.viewContext])
+        }
+        
+    }
+    
+    //方法: 批量删除所有数据。（创建两个请求，调用前面的 delete 方法）
+    func deleteAll(){
+        
+        //创建 tag 的批量删除请求，并执行
+        let request1: NSFetchRequest<NSFetchRequestResult> = Tag.fetchRequest()
+        delete(request1)
+        
+        //创建 issue 的批量删除请求，并执行
+        let request2: NSFetchRequest<NSFetchRequestResult> = Issue.fetchRequest()
+        delete(request2)
+        
+        //保存更改
+        save()
+        
+    }
+    
+    
+    
+    
+}
